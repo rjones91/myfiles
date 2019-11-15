3bb1e27f32a7dbb15b4ec8a3264ccc6775d14fca
rjones91 <57777939+rjones91@users.noreply.github.com>
2019-11-14 19:58:14 -0500

    added website files

---
+++ R_Jones_ Website/Scripts/myFunctions.js
@@ -0,0 +1,9 @@
+/* Raymond Jones
+10 Nov 2019
+My function list
+*/
+
+// Thank you for your submission
+function tysubmisAlert() {
+	alert("Thank you for your request for more information. We will contact you soon.")
+	}
